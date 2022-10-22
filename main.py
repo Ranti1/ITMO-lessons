91315a901eee7b35c089dbc6c5f29d85b322c612
79817 <almen69@yandex.ru>
2022-10-22 18:20:14 +0300

    new

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def summ_args(*args):
-    print(sum(args))
+"""def summ_args(*args):
+    array = [args]
+    for i in range(len(array)):
+        if array[i] == int:
+            print(1)
+        else:
+            print("type Error")
 
 
-summ_args(1, 2, 3, 700)
+summ_args(1, 3, 13, 131, 1)
+"""
 
