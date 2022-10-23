552f5060c2ae2e053f1221339738649a42517568
79817 <almen69@yandex.ru>
2022-10-23 15:34:46 +0300

    sum of args and check for palindrome

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-"""def summ_args(*args):
-    array = [args]
-    for i in range(len(array)):
-        if array[i] == int:
-            print(1)
+def summ_args(*args):
+    suma = 0
+    for ch in args:
+        if type(ch) == int:
+            suma += ch
         else:
-            print("type Error")
+            print(f"Error: bad type of variable {ch} - {type(ch)}, not 'int'")
+            break
+    print(f"Sum of the variables is {suma}")
 
 
-summ_args(1, 3, 13, 131, 1)
-"""
+summ_args(1, 3, 4, 131)
+summ_args("1", 3, 13, 131, "3113")
+
+
+def check_stroka(stroka):
+    slovo = ""
+    for i in range(len(stroka)):
+        word = stroka[-i - 1]
+        slovo += word
+    if stroka == slovo:
+        print(f"Слово {stroka} является палиндромом.")
+    else:
+        print(f"Слово {stroka} НЕ является палиндромом.")
+
+
+check_stroka("казак")
+check_stroka("холод")
+
 
