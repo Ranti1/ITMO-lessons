b59963bc72138023122560ef279f99e70a643545
79817 <almen69@yandex.ru>
2023-01-14 18:26:20 +0300

    Errors and try e.t.c

--- main.py
+++ main.py
@@ -2,15 +2,23 @@ def repeat(func):
     def wrapper():
         for i in range(3):
             print(f"{i + 1}. {func()}")
+
     return wrapper()
 
 
-@repeat
 def square(*args):
-    summ = 0
-    for num in args:
-        summ += num ** 2
-    return summ
+    try:
+        summ = 0
+        for num in args:
+            summ += num ** 2
+        return summ
+    except TypeError:
+        summ = 0
+        for num in args:
+            summ += int(num) ** 2
+        return summ
+    except ValueError:
+        return TypeError()
 
 
-square(1, 2, 3)
+print(square(1, "2", "3"))
